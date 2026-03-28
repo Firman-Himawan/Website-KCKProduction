08e63ea0a9b5198e94b7e0f08a869fc8bd963840
Firman Himawan <firmanhimawan007@gmail.com>
2026-03-28 22:54:11 +0700

    Update vite.config.ts

--- KCK-Production/vite.config.ts
+++ KCK-Production/vite.config.ts
@@ -3,19 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    allowedHosts: true,
-    hmr: {
-      overlay: false,
-    },
-  },
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
+  },
+  build: {
+    outDir: "dist",
   },
 });
